64527177078f711cedd573af8314cdd6572fb293
Jonathan Harrell <harr041@gmail.com>
2019-04-07 19:11:21 -0500

    fix contact us

--- wp-content/themes/case-unsolved/partials/content-contact.php
+++ wp-content/themes/case-unsolved/partials/content-contact.php
@@ -3,6 +3,6 @@
         <h2 class="contact-us-title">
             Want to know more about <em>Case Unsolved?</em>
         </h2>
-        <a href="" class="button">Contact us</a>
+        <a href="<?php echo esc_url(home_url('/about')); ?>" class="button">Contact us</a>
     </div>
 </section>
