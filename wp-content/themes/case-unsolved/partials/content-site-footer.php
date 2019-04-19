b9be0a6029af443ad3936adb8147467887915895
Jonathan Harrell <harr041@gmail.com>
2019-04-19 15:56:19 -0500

    add social links

--- wp-content/themes/case-unsolved/partials/content-site-footer.php
+++ wp-content/themes/case-unsolved/partials/content-site-footer.php
@@ -1,5 +1,13 @@
 <footer class="site-footer">
     <div class="container">
         <p>Copyright © 2018 by Case Unsolved, LLC. All rights reserved.</p>
+        <div class="social-links">
+            <a href="https://www.facebook.com/caseunsolvedproject/" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.svg" alt="Facebook">
+            </a>
+            <a href="https://www.instagram.com/case_unsolved/" target="_blank" rel="noopener noreferrer">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="Instagram">
+            </a>
+        </div>
     </div>
 </footer>
